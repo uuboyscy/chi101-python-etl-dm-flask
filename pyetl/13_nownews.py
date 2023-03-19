f34d5866280b2d7d2d60b2b8bd0910a4491a507b
uuboyscy <aegis12321@gmail.com>
2023-03-19 10:12:47 +0800

    Update: nownews download images

--- pyetl/13_nownews.py
+++ pyetl/13_nownews.py
@@ -1,8 +1,12 @@
 import json
 import pprint
+import os
 
 import requests
 
+folder_path = "./photo"
+if not os.path.exists(folder_path):
+    os.mkdir(folder_path)
 
 url = "https://www.nownews.com/nn-client/api/v1/cat/column/?pid=6085672"
 
@@ -14,7 +18,6 @@ res = requests.get(url, headers=headers)
 
 # json_data = json.loads(res.text)
 # pprint.pprint(json_data)
-
 # pprint.pprint(res.json())
 
 json_data = res.json()
@@ -23,7 +26,15 @@ for article_obj in json_data["data"]["newsList"]:
     post_title = article_obj["postTitle"]
     post_url = "https://www.nownews.com" + article_obj["postUrl"]
     image_url = article_obj["imageUrl"]
+    image_concent = requests.get(image_url, headers=headers).content
+    image_type = image_url.split('.')[-1].split('?')[0].replace('/', '-')
+    image_path = f"{folder_path}/{post_title}.{image_type}"
+
+    with open(image_path, "wb") as f:
+        f.write(image_concent)
+
     print(post_title)
     print(post_url)
     print(image_url)
+    print(image_path)
     print("=======")
