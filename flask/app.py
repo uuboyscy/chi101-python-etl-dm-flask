5f040e704e798480a9d1f8320b1d28327d07f70d
uuboyscy <aegis12321@gmail.com>
2023-04-15 13:54:02 +0800

    Update: Query string parameters

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -17,15 +17,44 @@ def greet(name):
 def two_sum(x, y):
     return str(int(x) + int(y))
 
-# [GET] /get_names/department_id:string/team_id:string
-@app.route("/get_names/<department_id>/<team_id>")
-def get_names(department_id, team_id):
+"""
+[GET] /get_names/department_id:string/team_id:int
+Response:
+    [
+        {
+            "emp_name": "Allen",
+            "emp_id": 13
+        },
+        {
+            "emp_name": "Ted",
+            "emp_id": 44
+        }, ...
+    ]
+"""
+@app.route("/get_names/<string:department_id>/<int:team_id>")
+def get_names(department_id: str, team_id: int) -> str:
     sql = """
         SELECT emp_name, emp_id FROM emp
         WHERE department_id = '{}'
         AND team_id = {};
     """.format(department_id, team_id)
     return sql
+
+
+# /hello_get?name=Allen&age=22
+@app.route("/hello_get")
+def hello_get():
+    name = request.args.get("name")
+    age = request.args.get("age")
+
+    if name is None:
+        output_html = "<h1>What's your name?</h1>"
+    elif age is None:
+        output_html = "<h1>Hello {}.</h1>".format(name)
+    else:
+        output_html = "<h1>Hello {}, you are {} years old.</h1>".format(name, age)
+
+    return output_html
 
 
 if __name__ == "__main__":
