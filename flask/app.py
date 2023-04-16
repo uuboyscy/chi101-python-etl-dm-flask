27b66a447acc88dc5de7b505b5e12673e3712b63
uuboyscy <aegis12321@gmail.com>
2023-04-16 14:05:14 +0800

    flask

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, render_template
 
-app = Flask(__name__)
+import poker as p
+import model
+from test_controller import test_controller
+
+app = Flask(__name__, static_url_path="/resource", static_folder="./my_resource")
+app.register_blueprint(test_controller, url_prefix="/controller_A")
 
 
 @app.route("/")
@@ -8,10 +13,14 @@ def hello_flask():
     return "<h1>Hello Flask!123123123123</h1>"
 
 
+# @app.route("/greet/<name>")
+# def greet(name):
+#     return "Hello {}".format(name)
+
+
 @app.route("/greet/<name>")
 def greet(name):
-    return "Hello {}".format(name)
-
+    return render_template("hello.html", name=name)
 
 @app.route("/two_sum/<x>/<y>")
 def two_sum(x, y):
@@ -42,19 +51,31 @@ def get_names(department_id: str, team_id: int) -> str:
 
 
 # /hello_get?name=Allen&age=22
+# @app.route("/hello_get")
+# def hello_get():
+#     name = request.args.get("name")
+#     age = request.args.get("age")
+#
+#     if name is None:
+#         output_html = "<h1>What's your name?</h1>"
+#     elif age is None:
+#         output_html = "<h1>Hello {}.</h1>".format(name)
+#     else:
+#         output_html = "<h1>Hello {}, you are {} years old.</h1>".format(name, age)
+#
+#     return output_html
+
+# /hello_get?name=Allen&age=22
 @app.route("/hello_get")
 def hello_get():
     name = request.args.get("name")
     age = request.args.get("age")
 
-    if name is None:
-        output_html = "<h1>What's your name?</h1>"
-    elif age is None:
-        output_html = "<h1>Hello {}.</h1>".format(name)
-    else:
-        output_html = "<h1>Hello {}, you are {} years old.</h1>".format(name, age)
-
-    return output_html
+    return render_template(
+        "hello_get.html",
+        name=name,
+        age=age,
+    )
 
 
 @app.route("/hello_post", methods=["GET", "POST"])
@@ -73,6 +94,21 @@ def hello_post():
         output_html += "<h1>Hello {}.</h1>".format(username)
 
     return output_html
+
+
+@app.route("/poker/<num>")
+def poker(num):
+    json_data = p.poker(int(num))
+
+    return jsonify(json_data)
+
+
+@app.route('/show_staff')
+def hello_google():
+    staff_data = model.getStaff()
+    column = ['ID', 'Name', 'DeptId', 'Age', 'Gender', 'Salary']
+    return render_template('show_staff.html', staff_data=staff_data,
+                                              column=column)
 
 
 if __name__ == "__main__":
